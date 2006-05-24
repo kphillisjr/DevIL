2384a7b70109d028bc9ee282526eb42ae0e77b5f
Meloni Dario <darkyojimbo@users.sourceforge.net>
2006-05-24 06:06:26 +0000

    removed ilGenImage and ilDeleteImage. Already present in ILU
    git-svn-id: https://openil.svn.sourceforge.net/svnroot/openil/branches/release_1_6_8@765 e624367d-d6e4-4977-a939-15c673ed3205

--- DevIL/src-IL/src/il_stack.c
+++ DevIL/src-IL/src/il_stack.c
@@ -54,12 +54,6 @@ ILvoid ILAPIENTRY ilGenImages(ILsizei Num, ILuint *Images)
 	return;
 }
 
-ILint ILAPIENTRY ilGenImage() {
-    ILuint i;
-    ilGenImages(1,&i);
-    return i;
-}
-
 //! Makes Image the current active image - similar to glBindTexture().
 ILvoid ILAPIENTRY ilBindImage(ILuint Image)
 {
@@ -142,11 +136,6 @@ ILvoid ILAPIENTRY ilDeleteImages(ILsizei Num, const ILuint *Images)
 			ilSetError(IL_ILLEGAL_OPERATION);
 		}*/
 	} while (++Index < (ILuint)Num);
-}
-
-
-ILvoid ILAPIENTRY ilDeleteImage(const ILuint Num) {
-    ilDeleteImages(1,&Num);
 }
 
 //! Checks if Image is a valid ilGenImages-generated image (like glIsTexture()).
